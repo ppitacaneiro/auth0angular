c79a950aaa31ffe80abbd51ae0cc563eb1d46ce8
pablo <ppitacaneiro@gmail.com>
2020-10-27 20:33:55 +0100

    Implementado loggin y logout con sdk de auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { PreciosComponent } from './components/precios/precios.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,11 @@ import { PreciosComponent } from './components/precios/precios.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    AuthModule.forRoot({
+      domain: 'dev-7kwyq-sx.eu.auth0.com',
+      clientId: 'hQZhDHCUjJTyM6WS1Z095zEG1qTCTFEW'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { 
+  
+  }
 
   ngOnInit(): void {
   }
